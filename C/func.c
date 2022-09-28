e9397bcdb0efe4ec132fed4def4e211a45c72011
sudoskys <coldlando@hotmail.com>
2022-09-29 00:01:16 +0800

    add

--- C/func.c
+++ C/func.c
@@ -2,8 +2,11 @@
 
 /* This is a comment. */
 int main(int argc, char *argv[])
+{
     int distance = 100;
     // this is also a comment
     printf("You are %d miles away.\n", distance);
     return 0;
 }
+
+
