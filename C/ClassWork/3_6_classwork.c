2a1d00ea700fa5b1de395d026b6430b242bead19
Jasmine <coldlando@hotmail.com>
2022-09-30 17:43:22 +0800

    add:code

---
+++ C/ClassWork/3_6_classwork.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+void morning() {
+	printf("I said,\"Good morning\"");
+}
+
+void hello() {
+	printf("c:\\program\\hello.c");
+}
+
+void work() {
+	printf("完成率为100%%");
+}
+int change(int a,int b) {
+	//
+	int temp;
+	temp = a;
+	a=b;
+	b=temp;
+	printf("%d,%d",a,b);
+	return 0;
+}
+int main(int argc,char *argv[]) {
+	int x=10, y=20;
+	change(x,y);
+}
