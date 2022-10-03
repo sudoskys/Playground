2649720978da9e60902ded3ad93080148e3bf7d1
sudoskys <coldlando@hotmail.com>
2022-10-03 21:48:15 +0800

    add:code

---
+++ C/ClassWork/math_test.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <math.h>
+
+
+// start
+int main(void){
+    double test_1,result;
+    scanf("%lf",&test_1);
+    result=sqrt(test_1);
+    printf("%lf\n",result);
+}
+
+
