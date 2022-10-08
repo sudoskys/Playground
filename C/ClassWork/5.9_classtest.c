9c69b088ed59ed758cbbc41854c3b3eb91e529d5
sudoskys <coldlando@hotmail.com>
2022-10-08 03:00:16 +0000

    yes

---
+++ C/ClassWork/5.9_classtest.c
@@ -0,0 +1,81 @@
+// 测试程序 
+#include <stdio.h>
+#include <math.h> 
+
+void test1(void){
+	//次方 
+	double x,y,result;
+	scanf("%lf,%lf",&x,&y);
+	result=pow(x,y);
+	printf("%.2lf",result);
+}
+
+void test2(){
+	// 暴力解法 
+	int a,b,c;
+	int a1,b1,c1;
+	scanf("%d%d%d",&a,&b,&c);
+	// 全部复制 
+	a1=a;
+	b1=b;
+	c1=c;
+	 
+	a=b1;
+	b=c1;
+	c=a1;
+	printf("a=%d,b=%d,c=%d",a,b,c);
+}
+
+void test3(){
+	// 逆序输出
+	int index,item;
+	char scr[3];
+	scanf("%s",scr);
+	int sum=0;
+	int haszero=1;
+	for (int i=0;i<sizeof(scr);i++){
+		//printf("run:%d\n",i);
+		index=sizeof(scr)-i-1;
+		item=scr[index];
+		
+		// 开始判定
+		if (item!='0' && haszero==1){
+			haszero=0;
+		}
+		
+		// 开始输出
+		if (haszero=1)
+			printf("");
+			//printf("%c",item);
+		else
+			printf("%d",item);
+		
+	}
+}
+
+void test4(){
+	// 采用简单方法 
+	signed int init;
+	scanf("%d",&init);
+	// int if 
+	if (init>5){
+	 // convert
+     	init = init-10;
+	}
+	printf("%d",init+4);
+}
+
+
+void test5(){
+	double money; 
+	int yuan,jiao,fen;
+	// 位数?
+	scanf("%lf",&money);
+	yuan=(money)/1.0;
+	jiao=(money*10-yuan*1)/100.0;
+	fen=(money*100-yuan*100-jiao*10)/1.0;
+	printf("%d元%d角%d分",yuan,jiao,fen);
+}
+int main(){
+	test3();
+}
--- README.md
+++ README.md
@@ -3,4 +3,8 @@
 零散代码/非项目代码
 
 
-[CodeServer-GPG签名问题](http://t.zoukankan.com/julymaple-p-14657149.html)
+[CodeServer-GPG签名问题](https://stackoverflow.com/questions/52808365/git-error-gpg-failed-to-sign-the-data-on-linux)
+
+```shell
+export GPG_TTY=$(tty)
+```
