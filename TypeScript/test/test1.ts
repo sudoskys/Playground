5af9fcfa095734507f462cdf3722ace45005b6c3
sudoskys <coldlando@hotmail.com>
2023-01-20 18:13:35 +0800

    init

---
+++ TypeScript/test/test1.ts
@@ -0,0 +1,30 @@
+// 接口
+interface Person {
+    firstName: string;
+    lastName: string;
+}
+
+// 学生类型
+class Student {
+    fullName: string;
+
+    constructor(public firstName: string,
+                public middleInitial: string,
+                public lastName: string
+    ) {
+        this.fullName = firstName + "" + middleInitial + "" + lastName;
+    }
+}
+
+
+function greeter(person: Person) {
+    return "Hello, " + person.firstName + " " + person.lastName;
+}
+
+// let user = {firstName: "Jane", lastName: "User"};
+
+let users = new Student("Jane", "M.", "User");
+
+
+document.body.innerHTML = greeter(users);
+
