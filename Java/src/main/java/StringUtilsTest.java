33fa4b8d092d31deb5c2784716339468eb58eba0
sudoskys <coldlando@hotmail.com>
2023-12-08 10:12:30 +0800

    feat(java)

---
+++ Java/src/main/java/StringUtilsTest.java
@@ -0,0 +1,8 @@
+import org.apache.commons.lang3.StringUtils;
+
+class StringUtilsTest {
+    public static void main(String[] args) {
+        String str = "Hello World";
+        System.out.println(StringUtils.reverse(str));
+    }
+}
