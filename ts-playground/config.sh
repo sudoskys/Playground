ef81aeaf107371846fbdaf0c0e8dc8f5e226fa42
sudoskys <coldlando@hotmail.com>
2024-03-04 11:29:17 +0800

    ✨♻️ Update tsconfig.json and add rollup.config.mjs
    - Update compilerOptions in tsconfig.json
    - Add rollup.config.mjs for Rollup bundler configuration

--- ts-playground/config.sh
+++ ts-playground/config.sh
@@ -1,3 +1,7 @@
+mkdir ~/.npm-global
+npm config set prefix '~/.npm-global'
+export PATH=~/.npm-global/bin:$PATH
+
 npm install ts-node
 npm install webpack webpack-cli
 npm install ts-loader source-map-loader
