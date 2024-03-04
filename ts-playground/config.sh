8af7dbf4704c29b33f57ee039ab434665e74a23e
sudoskys <coldlando@hotmail.com>
2024-03-04 10:00:48 +0800

    ✨🔧 Add initial project configuration script
    ⚙️ Added initial project configuration script for npm packages and dependencies.

---
+++ ts-playground/config.sh
@@ -0,0 +1,5 @@
+npm install ts-node
+npm install webpack webpack-cli
+npm install ts-loader source-map-loader
+npm install --save react react-dom @types/react @types/react-dom
+npm install --save-dev typescript awesome-typescript-loader source-map-loader
