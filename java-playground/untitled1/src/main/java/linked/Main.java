50e0dfcd6a84f2b1f4717eda4ddb6cca70741d17
sudoskys <coldlando@hotmail.com>
2024-03-08 09:41:37 +0800

    :sparkles: Add Java code for manipulating collections and stamps :coffee:

---
+++ java-playground/untitled1/src/main/java/linked/Main.java
@@ -0,0 +1,39 @@
+package linked;
+
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.Scanner;
+import java.util.TreeSet;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner nc = new Scanner(System.in);
+        String input = nc.nextLine();
+        System.out.println(checkBrackets(input));
+        TreeSet<Integer> set1 = new TreeSet<>();
+        HashSet<Integer> set2 = new HashSet<>();
+
+    }
+
+    public static String checkBrackets(String input) {
+        LinkedList<Character> stack = new LinkedList<>();
+        int count = 0;
+        for (char c : input.toCharArray()) {
+            if (c == '[') {
+                stack.push(c);
+            } else if (c == ']') {
+                if (stack.isEmpty()) {
+                    return "期望[";
+                } else {
+                    stack.pop();
+                    count++;
+                }
+            }
+        }
+        if (!stack.isEmpty()) {
+            return "期望]";
+        } else {
+            return "匹配\n" + count;
+        }
+    }
+}
