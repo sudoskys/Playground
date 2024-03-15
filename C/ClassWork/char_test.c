c31165119b5f46aef4793016109618fa739e9ad8
sudoskys <coldlando@hotmail.com>
2024-03-15 10:30:43 +0800

    ⚙️ refactor: Move files to week03 directory
    ℹ️ Renamed files to move them to the week03 directory for better organization.

--- C/ClassWork/char_test.c
+++
@@ -1,9 +0,0 @@
-//@test字符类型
-#include <stdio.h>
-// 测试 char 类型
-int main(int argc, char *argv[])
-{
-    char ch='B';
-    printf("%c,%d",ch,ch);
-    return 0;
-}
--- C/ClassWork/type.c
+++
@@ -1,7 +0,0 @@
-//@类型测试
-#include <stdio.h>
-
-int main(int argc, char *argv[]){
-        double z = 50.3;
-        printf("z=%f",z);
-}
