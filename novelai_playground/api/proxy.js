fca14a1b2fe91921247d5224b1f708ad9cebed13
sudoskys <coldlando@hotmail.com>
2024-04-02 00:35:52 +0800

    ⚙️ chore(api): Configure vercel proxy for backend
    - Added vercel.json configuration file to set up proxy for backend requests
    - Created api/proxy.js file to handle vercel serverless function for proxying backend requests
    
    #playground #vercel #proxy #backend #configuration

--- novelai_playground/api/proxy.js
+++ novelai_playground/api/proxy.js
@@ -1,22 +1,16 @@
 // api/proxy.js
-// 该服务为 vercel serve跨域处理
-const {createProxyMiddleware} = require('http-proxy-middleware')
+import {createProxyMiddleware} from 'http-proxy-middleware';
 
-module.exports = (req, res) => {
-    let target = ''
-    // 代理目标地址
-    // 这里使用 backend 主要用于区分 vercel serverless 的 api 路径
+export default (req, res) => {
+    let target = '';
     if (req.url.startsWith('/backend')) {
-        target = 'https://api.novelai.net'
+        target = 'https://api.novelai.net';
     }
-    // 创建代理对象并转发请求
     createProxyMiddleware({
         target,
         changeOrigin: true,
         pathRewrite: {
-            // 通过路径重写，去除请求路径中的 `/backend`
-            // 例如 /backend/user/login 将被转发到 https://fanyi-api.baidu.com/user/login
             '^/backend/': '/',
         },
-    })(req, res)
-}
+    })(req, res);
+};
