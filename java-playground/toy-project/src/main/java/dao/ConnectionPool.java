ea8efe9fa83e8bc933455830f2f8302a1237a632
sudoskys <coldlando@hotmail.com>
2024-06-17 10:23:39 +0800

    update

--- java-playground/toy-project/src/main/java/dao/ConnectionPool.java
+++ java-playground/toy-project/src/main/java/dao/ConnectionPool.java
@@ -3,18 +3,30 @@ package dao;
 import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.SQLException;
-
+import java.util.Properties;
 
 public class ConnectionPool {
     private static final HikariConfig config = new HikariConfig();
     private static final HikariDataSource ds;
 
     static {
-        config.setJdbcUrl("jdbc:postgresql://localhost:5432/postgres");
-        config.setUsername("postgresUser");
-        config.setPassword("postgresPW");
+        Properties props = new Properties();
+        try (InputStream is = ConnectionPool.class.getClassLoader().getResourceAsStream("db.properties")) {
+            if (is != null) {
+                props.load(is);
+                config.setJdbcUrl(props.getProperty("jdbcUrl"));
+                config.setUsername(props.getProperty("username"));
+                config.setPassword(props.getProperty("password"));
+            } else {
+                throw new RuntimeException("Cannot find db.properties file");
+            }
+        } catch (IOException e) {
+            throw new RuntimeException("Failed to load db.properties file", e);
+        }
         config.setMaximumPoolSize(10);
         ds = new HikariDataSource(config);
     }
@@ -22,13 +34,6 @@ public class ConnectionPool {
     private ConnectionPool() {
     }
 
-    /**
-     * Returns a connection to the database
-     *
-     * @return Connection to the database
-     * @throws SQLException if a database access error occurs
-     * @see Connection
-     */
     public static Connection getConnection() throws SQLException {
         return ds.getConnection();
     }
