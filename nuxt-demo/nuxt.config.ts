b3c506353c308efa1d1c9fbbf20db1f94e27e366
sudoskys <coldlando@hotmail.com>
2024-09-30 11:11:37 +0800

    init

---
+++ nuxt-demo/nuxt.config.ts
@@ -0,0 +1,11 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+    compatibilityDate: '2024-04-03',
+    devtools: {enabled: true},
+    runtimeConfig: {
+        apiSecret: '123',
+        public: {
+            apiBase: '/api'
+        }
+    }
+})
---
+++ nuxt-demo/pages/index.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+const runtimeConfig = useRuntimeConfig()
+</script>
+
+<template>
+
+  <div>
+    <NuxtRouteAnnouncer/>
+    <div>
+      pages/index.vue
+      <h1>{{ runtimeConfig.title }}</h1>
+      <p>{{ runtimeConfig.description }}</p>
+    </div>
+  </div>
+
+</template>
+
+<style scoped>
+
+</style>
