80bac895d7bc9c9e61a4e037bd15267376e04539
sudoskys <coldlando@hotmail.com>
2024-11-21 00:50:57 +0800

    fix link

--- springboot-java/src/main/java/com/star/demo/exception/InvalidPasswordException.java
+++
@@ -1,7 +0,0 @@
-package com.star.demo.exception;
-
-public class InvalidPasswordException extends RuntimeException {
-    public InvalidPasswordException(String message) {
-        super(message);
-    }
-}
